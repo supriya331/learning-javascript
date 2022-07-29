2f52b742f6ffaa5faf2362e1235f26ccbdcb4504
SUPRIYA GUPTA <100029756+supriya331@users.noreply.github.com>
2022-07-28 20:40:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+/*var a = 1;
+var b = "1";
+if(a==b){
+    console.log("both are equal");
+}else{
+    console.log("both are not equal");
+}
+console.log("supriya gupta");
+var a = 10;
+var b = 21;
+var c = 7;
+var sum = a + b;
+var c = c + sum;
+console.log(sum);
+console.log("<br>");
+console.log(c);*/
+/*var day  = 5;
+switch (day) {
+    case 0:
+        console.log("today is sunday");
+        break;
+    case 1:
+        console.log("today is monday");
+        break;
+    case 2:
+        console.log("today is tuesday");
+        break;
+    case 3:
+        console.log("today is wednesday");
+        break;
+    case 4:
+        console.log("today is thursday");
+        break;
+    default: console.log("enter a valid week day");    
+
+}*/
+//var i = 1;
+//while(i<=10){
+ //   console.log(i);
+ //   i++;
+   // console.log(i);
+//}
+/*var i = 2;
+var j = 1;
+while(i<=20){
+    console.log("2"+"*"+j+" = " +i);
+    i = i + 2;
+    j++;
+    console.log("<br>");
+}*/
+console.log("my name is javascript");
+//do-while loop
+var i = 0;
+do
+{
+    console.log(i);
+    i++;
+}while(i<5);
+
+var k = 0;
+do
+{
+    if(k==3){
+        break;
+    }
+    console.log(k);
+    k++;
+    console.log("<br>");
+}while(true);
